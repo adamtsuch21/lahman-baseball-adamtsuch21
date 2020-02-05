b8ddb55c1f021efbc8c36841257cfa8b5ca41cdd
Adam Tsuchiyama <adamtsuch21@gmail.com>
2020-02-04 22:40:44 -0600

    readded files

---
+++ queries/lahman_question_3.sql
@@ -0,0 +1,55 @@
+/*
+    QUESTION ::
+        * Find all players in the database who played 
+		  at Vanderbilt University. Create a list 
+		  showing each player’s first and last names as
+		  well as the total salary they earned in the 
+		  major leagues. Sort this list in descending 
+		  order by the total salary earned. Which 
+		  Vanderbilt player earned the most money in 
+		  the majors?
+		
+    SOURCES ::
+        * schoools, collegeplaying, people, salaries
+
+    DIMENSIONS ::
+        * playerid, firstname, lastname
+
+    FACTS ::
+        * total_salary
+    FILTERS ::
+        * schoolid = 'vandy'
+
+    DESCRIPTION ::
+        * First I had to join schools and 
+		  collegeplaying on schoolid to match playerid 
+		  with their schools. Then I joined people on 
+		  playerid to get the firstname, lastname of 
+		  those players. Then I joined salaries on 
+		  playerid to get the salaries of those players
+		  (using SUM to get the total_salary for each 
+		  player and selecting distinct player_ids). 
+		  I then filtered to only show players with a 
+		  schoolid of 'vandy' for Vanderbilt University.
+		  I then grouped by playerid, firstname, and 
+		  lastname and ordered by total_salary in 
+		  descending order to have the player with the 
+		  highest total_salary on top.
+
+    ANSWER ::
+        David Price
+
+*/
+
+SELECT DISTINCT p.playerid AS player_id, namefirst AS first_name,
+       namelast AS last_name, SUM(salary) as total_salary
+FROM schools AS sch
+JOIN collegeplaying AS cp
+ON sch.schoolid = cp.schoolid
+JOIN people AS p
+ON cp.playerid = p.playerid
+JOIN salaries AS s
+ON p.playerid = s.playerid
+WHERE sch.schoolid = 'vandy'
+GROUP BY p.playerid, first_name, last_name
+ORDER BY total_salary DESC;
---
+++ queries/lahman_question_8.sql
@@ -0,0 +1,81 @@
+/*
+    QUESTION ::
+        * Using the attendance figures from the homegames table,
+		  find the teams and parks which had the top 5 average
+		  attendance per game in 2016 (where average attendance
+		  is defined as total attendance divided by number of 
+		  games). Only consider parks where there were at least
+		  10 games played. Report the park name, team name, and
+		  average attendance. Repeat for the lowest 5 average 
+		  attendance.
+
+    SOURCES ::
+        * homegames, parks
+
+    DIMENSIONS ::
+        * park_name, team
+
+    FACTS ::
+        * avg_att
+
+    FILTERS ::
+        * Only parks that played at least 10 games in 2016.
+
+    DESCRIPTION ::
+        For the parks with the highest attendance, I joined the
+		homegames table with the parks table (on parks) to get
+		the park_name. I then did a where statement to filter for
+		only parks where 10 or more games were played for the year
+		2016. I then calculated avg_att(attendance/games) and
+		ordered by avg_att in descending order with a limit of 5.
+		For the parks with the lowest attendance, I did the same 
+		thing but ordered avg_att in ascending order.
+    
+    ANSWER ::
+        * Top 5 highest attendance:
+			1. Dodger Stadium, 45719.90 avg_att
+			2. Busch Stadium III, 42524.57 avg_att
+			3. Rogers Centre, 41877.77 avg_att
+			4. AT&T Park, 41546.37 avg_att
+			5. Wrigley Field, 39906.42 avg_att
+			
+		  Top 5 lowest attendance:
+			1. Tropicana Field, 15878.56 avg_att
+			2. Oakland-Alameda County Coliseum, 18784.02 avg_att
+			3. Progressive Field, 19650.21 avg_att
+			4. Marlins Park, 21405.21 avg_att
+			5. U.S. Cellular Field, 21559.17 avg_att
+
+*/
+
+--Parks with highest avg_att
+SELECT team, park_name, 
+       ROUND(
+		     CAST(
+			      attendance::float/games::float AS numeric
+		         ), 2
+		     ) AS avg_att
+FROM homegames AS hg
+JOIN parks AS p
+ON hg.park = p.park
+WHERE games >= 10
+AND year = 2016
+GROUP BY team, park_name, attendance, games
+ORDER BY avg_att DESC
+LIMIT 5;
+
+--Parks with lowest avg_att
+SELECT team, park_name, 
+       ROUND(
+		     CAST(
+			      attendance::float/games::float AS numeric
+		         ), 2
+		     ) AS avg_att
+FROM homegames AS hg
+JOIN parks AS p
+ON hg.park = p.park
+WHERE games >= 10
+AND year = 2016
+GROUP BY team, park_name, attendance, games
+ORDER BY avg_att
+LIMIT 5;
